9d31fd48071af1756c82115da829e0c1c8d107ab
Samelly Snertz <samelly.snertz@lucastechnologyservice.com>
2024-05-01 11:00:25 +0000

    "feat(#1543): Building Domain Entities"

---
+++ src/Modules/CloudSuite.BackBonne.Domain/models/Administration/Country.cs
@@ -0,0 +1,14 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.models.Administration
+{
+    public class Country : Entity, IAggregateRoot
+    {
+        public string? CountryName { get; private set; }
+    }
+}
