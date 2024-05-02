c7d78ad680643d2a11b9809b7bb3891eaf668d9d
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-05-02 06:06:58 -0300

    "feat(#1543): Building Domain entities"

---
+++ src/Modules/CloudSuite.BackBonne.Domain/models/BankIntegration/CostCenter.cs
@@ -0,0 +1,20 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.models.BankIntegration
+{
+    public class CostCenter : Entity, IAggregateRoot
+    {
+        public string? COD_CENTRO_CUSTO { get; private set; }
+
+        public string? Description { get; private set; }
+
+        public int? ID_CENTRO_CUSTO_PAI { get; private set; }
+
+        public string? Cod_Desc { get; private set; }
+    }
+}
