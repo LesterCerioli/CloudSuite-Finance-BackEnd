1b4ad8b8e56c8e061ac32885c29c84f00c8848c2
Samelly Snertz <samelly.snertz@lucastechnologyservice.com>
2024-05-09 00:18:53 +0000

    "feat(#1543): Building View models"

---
+++ src/Modules/CloudSuite.BackBonne.Application/ViewModels/AccountViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Application.ViewModels
+{
+    public class AccountViewModel
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        [DisplayName("AccountNumber")]
+        [MinLength(03)]
+        [MaxLength(14)]
+        public string? AccountNumber { get; set; }
+
+        [DisplayName("AccountDigit")]
+        public int? AccountDigit { get; set; }
+    }
+}
---
+++ src/Modules/CloudSuite.BackBonne.Application/ViewModels/BankViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Application.ViewModels
+{
+    public class BankViewModel
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        [DisplayName("CodeBank")]
+        [MaxLength(3)]
+        public int? Codebank { get; set; }
+
+        [DisplayName("BankName")]
+        [MaxLength(50)]
+        public string? BankName { get; set; }
+    }
+}
