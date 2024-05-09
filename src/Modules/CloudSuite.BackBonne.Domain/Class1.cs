46a760fc16895c62246184399c83cb6c414f837d
Samelly Snertz <samelly.snertz@lucastechnologyservice.com>
2024-05-09 00:03:47 +0000

    "feat(#1543): Building Domain entities"

--- src/Modules/CloudSuite.BackBonne.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CloudSuite.BackBonne.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Modules/CloudSuite.BackBonne.Domain/models/Accounts Context/Account.cs
@@ -0,0 +1,22 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.models.Accounts_Context
+{
+    public class Account : Entity, IAggregateRoot
+    {
+        public Account(string? accountNumber, int accountDigit)
+        {
+            AccountNumber = accountNumber;
+            AccountDigit = accountDigit;
+        }
+
+        public string? AccountNumber { get; private set; }
+
+        public int AccountDigit { get; private set; }
+    }
+}
