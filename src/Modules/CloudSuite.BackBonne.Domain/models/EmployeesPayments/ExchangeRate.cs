b74ad81924269b46ff9e1eedaf64c80ac5ba8525
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-06-19 12:55:11 -0700

    "building Finance entities"

--- src/Modules/CloudSuite.BackBonne.Domain/models/EmployeesPayments/ExchangeRate.cs
+++ src/Modules/CloudSuite.BackBonne.Domain/models/EmployeesPayments/ExchangeRate.cs
@@ -9,6 +9,17 @@ namespace CloudSuite.BackBonne.Domain.models.EmployeesPayments
 {
     public class ExchangeRate : Entity, IAggregateRoot
     {
+        public ExchangeRate(string? currency, decimal? valueInBRL, decimal? valueInUS, decimal? valueInEuro, DateTime? lastUpdated, decimal? dollarReference, decimal? euroRefference)
+        {
+            Currency = currency;
+            ValueInBRL = valueInBRL;
+            ValueInUS = valueInUS;
+            ValueInEuro = valueInEuro;
+            LastUpdated = lastUpdated;
+            DollarReference = dollarReference;
+            EuroRefference = euroRefference;
+        }
+
         public string? Currency { get; private set; } // Ex: BRL, USD, EUR
 
         public decimal? ValueInBRL { get; private set; } // Valor de 1 unidade da moeda em BRL
@@ -18,5 +29,11 @@ namespace CloudSuite.BackBonne.Domain.models.EmployeesPayments
         public decimal? ValueInEuro { get; private set; } // Valor de 1 unidade da moeda em Euro
 
         public DateTime? LastUpdated { get; private set; }
+
+        //Valor de referência do Dólar em Reais
+        public decimal? DollarReference { get; private set; }
+
+        //Valor de refrência do Euro em dólares
+        public decimal? EuroRefference { get; private set; }
     }
 }
