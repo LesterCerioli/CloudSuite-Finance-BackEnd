acc4362f5bc53195c4c12cf86c95c56676970a32
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-19 17:41:20 -0300

    feat(#1657): typo fix

--- src/Modules/CloudSuite.BackBonne.Application/Handlers/DocumentBills/Invoices/Requests/CheckInvoiceExistsByAmountRequest.cs
+++ src/Modules/CloudSuite.BackBonne.Application/Handlers/DocumentBills/Invoices/Requests/CheckInvoiceExistsByAmountRequest.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using CloudSuite.BackBonne.Application.Handlers.DocumentBills.Invoices.Responses;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
