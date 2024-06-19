4faea91fcc7e0368ff10c45becd0f28b5542e611
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-19 14:15:00 -0300

    feat(#1657): building interface from invoice and payment

---
+++ src/Modules/CloudSuite.BackBonne.Domain/contracts/DownloadBills/IPaymentRepository.cs
@@ -0,0 +1,27 @@
+﻿using CloudSuite.BackBonne.Domain.models.DownloadBills;
+using CloudSuite.Modules.Commons.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.contracts.DownloadBills
+{
+    public interface IPaymentRepository
+    {
+        Task<Payment> GetByAmount(decimal amount);
+
+        Task<Payment> GetByPaymentDate(DateTime paymentDate);
+
+        Task<Payment> GetByPaymentMethod(PaymentMethod paymentMethod);
+
+        Task<IEnumerable<Payment>> GetList();
+
+        Task Add(Payment supplier);
+
+        void Update(Payment supplier);
+
+        void Remove(Payment supplier);
+    }
+}
