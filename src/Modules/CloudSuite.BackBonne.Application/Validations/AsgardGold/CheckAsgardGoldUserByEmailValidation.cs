5093081b3dd6e7bba0fa0cfd4255fb9ef09baa86
“LucasFranczak” <lucasfranczak@gmail.com>
2024-06-26 14:05:07 -0300

    feat(#1660): Building Asgard Gold User Handlers

--- src/Modules/CloudSuite.BackBonne.Application/Validations/AsgardGold/CheckAsgardGoldUserByEmailValidation.cs
+++ src/Modules/CloudSuite.BackBonne.Application/Validations/AsgardGold/CheckAsgardGoldUserByEmailValidation.cs
@@ -7,5 +7,17 @@ namespace CloudSuite.BackBonne.Application.Validations.AsgardGold
 {
     public class CheckAsgardGoldUserByEmailValidation : AbstractValidator<CheckAsgardGoldUserByEmailRequest>
     {
+        public CreateAsgardGoldUserValidation()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("O campo de e-mail é obrigatório.")
+                .EmailAddress()
+                .WithMessage("O campo de e-mail deve ser um endereço de e-mail válido.")
+                .Length(1, 100)
+                .WithMessage("O campo de e-mail deve ter entre 1 e 100 caracteres.")
+                .NotNull()
+                .WithMessage("O campo de e-mail não pode ser nulo.");
+        }
     }
 }
--- src/Modules/CloudSuite.BackBonne.Application/Validations/UserClaim/CheckUserClaimByTypeValidation.cs
+++ src/Modules/CloudSuite.BackBonne.Application/Validations/UserClaim/CheckUserClaimByTypeValidation.cs
@@ -6,5 +6,15 @@ namespace CloudSuite.BackBonne.Application.Validations.UserClaim
 {
     public class CheckUserClaimByTypeValidation : AbstractValidator<CheckUserClaimByTypeRequest>
     {
+        public CheckUserClaimByTypeValidation()
+        {
+            RuleFor(x => x.Type)
+                .NotEmpty()
+                .WithMessage("O campo 'Type' é obrigatório.")
+                .Length(1, 50)
+                .WithMessage("O campo 'Type' deve ter entre 1 e 50 caracteres.")
+                .NotNull()
+                .WithMessage("O campo 'Type' não pode ser nulo.");
+        }
     }
 }
