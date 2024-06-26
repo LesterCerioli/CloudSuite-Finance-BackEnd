692b8f6fa7e2fea846dd1201ea603d8ecdf69cce
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-06-26 15:24:20 -0300

    feat(#1657): adding contracts and core files

---
+++ src/Modules/CloudSuite.BackBonne.Domain/contracts/DownloadBills/IInvoiceRepository.cs
@@ -0,0 +1,29 @@
+﻿using CloudSuite.BackBonne.Domain.models.DownloadBills;
+using CloudSuite.Modules.Commons.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.contracts.DownloadBills
+{
+    public interface IInvoiceRepository
+    {
+        Task<Invoice> GetByAmount(decimal? amount);
+
+        Task<Invoice> GetByDueDate(DateTime? dueDate);
+
+        Task<Invoice> GetByPaymentDate(DateTimeOffset? paymentDate);
+
+        Task<Invoice> GetByInvoiceStatus(InvoiceStatus? status);
+
+        Task<IEnumerable<Invoice>> GetList();
+
+        Task Add(Invoice supplier);
+
+        void Update(Invoice supplier);
+
+        void Remove(Invoice supplier);
+    }
+}
---
+++ src/Modules/CloudSuite.BackBonne.Domain/contracts/DownloadBills/IPaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using CloudSuite.BackBonne.Domain.models.DownloadBills;
+using CloudSuite.Modules.Commons.Enumerators;
+
+namespace CloudSuite.BackBonne.Domain.contracts.DownloadBills
+{
+    public interface IPaymentRepository
+    {
+        Task<Payment> GetByAmount(decimal? amount);
+
+        Task<Payment> GetByPaymentDate(DateTime? paymentDate);
+
+        Task<Payment> GetByPaymentMethod(PaymentMethod? paymentMethod);
+
+        Task<IEnumerable<Payment>> GetList();
+
+        Task Add(Payment supplier);
+
+        void Update(Payment supplier);
+
+        void Remove(Payment supplier);
+    }
+}
