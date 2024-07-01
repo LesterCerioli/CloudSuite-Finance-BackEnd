0c1275deb2a2125e5a2ebe94e74cba18cec5fcb5
artur-jansen <artur.jansenas@gmail.com>
2024-07-01 15:01:16 -0300

    feat(#1659): Build handlers

---
+++ src/Modules/CloudSuite.BackBonne.Application/Handlers/Administration/EducationLevel/CreateEducationLevelCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Application.Handlers.Administration.EducationLevel
+{
+    internal class CreateEducationLevelCommand
+    {
+    }
+}
