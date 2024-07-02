73a72d12ea39fa954adcc804640210fb52555746
artur-jansen <artur.jansenas@gmail.com>
2024-07-02 14:21:06 -0300

    feat(#1684): Build Entitie and Contract

---
+++ src/Modules/CloudSuite.BackBonne.Domain/contracts/AdviceService/IInvestmentRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.BackBonne.Domain.models.AdviceService
+{
+    public interface IInvestmentRepository 
+    {
+        Task<Investment> GetByTaxPayerRegistry(string taxPayerRegistry);
+
+        Task<Investment> GetByName(string name);
+
+        Task<Investment> GetByInvestmentProfile(string investmentProfile);
+
+        Task<IEnumerable<Investment>> GetList();
+
+        Task Add(Investment investment);
+
+        void Update(Investment investment);
+        
+        void Remove(Investment investment);
+    }
+}
---
+++ src/Modules/CloudSuite.BackBonne.Domain/models/AdviceService/Investment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using NetDevPack.Domain;
+
+namespace CloudSuite.BackBonne.Domain.models.AdviceService
+{
+    public class Investment : Entity, IAggregateRoot
+    {
+        public Investment(string taxPayerRegistry, string name, string investmentProfile)
+        {
+            TaxPayerRegistry = taxPayerRegistry;
+            Name = name;
+            InvestmentProfile = investmentProfile;
+        }
+        public Investment() { }
+
+        public string? TaxPayerRegistry { get; private set; }
+
+        public string? Name { get; private set; }
+
+        public string? InvestmentProfile { get; private set; }
+    }
+}
